75c04f7105f70147a5ec10fdfc034a574b466114
Allef Luziano <allef.luziano@alock.com.br>
2020-06-24 19:31:09 -0300

    refatorando javascript, e implementando atualização de quantidade do item na view de carrinho

---
+++ Ecommerce/wwwroot/js/carrinho.js
@@ -0,0 +1,43 @@
+﻿class Carrinho {
+    clickIncremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade++;
+        this.postQuantidade(data);    
+        setTimeout(function () { location.reload() }, 500);
+    }
+
+    clickDecremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade--;
+        this.postQuantidade(data);
+        setTimeout(function () { location.reload() }, 500);
+    }
+
+    getData(elemento) {
+        let linhaDoItem = $(elemento).parents('[item-id]');
+        let itemId = parseInt($(linhaDoItem).attr('item-id'));
+        let novaQtde = parseInt($(linhaDoItem).find('input').val());
+
+        return {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+    }
+
+    postQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+    }
+}
+
+var carrinho = new Carrinho();
+
+
+
+
+
+
